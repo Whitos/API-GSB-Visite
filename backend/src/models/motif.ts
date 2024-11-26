9da12c39619453b567387e7cf48088f570b2f92f
Pavel <pcpvts@gmail.com>
2024-11-26 23:59:33 +0100

    new project

--- backend/src/models/motif.ts
+++ backend/src/models/motif.ts
@@ -2,10 +2,12 @@ import mongoose, { Schema, Document } from "mongoose";
 
 interface IMotif extends Document {
   libelle: string;
+  description?: string;  // Rend description optionnel
 }
 
 const MotifSchema: Schema = new Schema({
   libelle: { type: String, required: true },
+  description: { type: String }
 });
 
 export default mongoose.model<IMotif>("Motif", MotifSchema);
